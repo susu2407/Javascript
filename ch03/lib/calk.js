a0d3816c0f7ad715aece22e2ee4a30f135d635ce
susu2407 <tndus159200297@gmail.com>
2025-08-28 11:40:59 +0900

    0828 3rd

---
+++ ch03/lib/calk.js
@@ -0,0 +1,15 @@
+function plus(x, y) {
+    return x + y;
+}
+
+function minus(x, y) {
+    return x - y;
+}
+
+function muli(x, y) {
+    return x * y;
+}
+
+function div(x, y) {
+    return x / y;
+}
